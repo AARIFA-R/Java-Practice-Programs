4bdb83c09859419a5b4387463603ffa97572da7b
AARIFA R <royalmdn02@gmail.com>
2026-07-23 15:27:06 +0530

    Add SmallestElement class to find minimum value

---
+++ 2_SmallestElement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 40, 30};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Smallest = " + min);
+    }
+}

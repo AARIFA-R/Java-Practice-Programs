78867d0f56edf6a7a8f1cda7b89f629bd3d87559
AARIFA R <royalmdn02@gmail.com>
2026-07-23 15:26:21 +0530

    Create 1_LargestElement.java

---
+++ 1_LargestElement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 40, 30};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        System.out.println("Largest = " + max);
+    }
+}

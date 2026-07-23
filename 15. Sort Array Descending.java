e20e1d9c62fe43af30c72436a73708da208d4f3a
AARIFA R <royalmdn02@gmail.com>
2026-07-23 16:03:12 +0530

    Update 15. Sort Array Descending.java

--- 15. Sort Array Descending.java
+++ 15. Sort Array Descending.java
@@ -4,7 +4,6 @@ public class DescendingSort {
     public static void main(String[] args) {
 
         Integer[] arr={5,2,1,4,3};
-
         Arrays.sort(arr, Collections.reverseOrder());
 
         System.out.println(Arrays.toString(arr));

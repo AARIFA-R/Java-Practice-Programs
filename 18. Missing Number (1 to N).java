146ecfc878b517a22665bbe02c303e6d61694390
AARIFA R <royalmdn02@gmail.com>
2026-07-23 15:36:13 +0530

    Create 18. Missing Number (1 to N).java

---
+++ 18. Missing Number (1 to N).java
@@ -0,0 +1,17 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,5};
+
+        int n=5;
+
+        int sum=n*(n+1)/2;
+
+        int arrSum=0;
+
+        for(int num:arr)
+            arrSum+=num;
+
+        System.out.println(sum-arrSum);
+    }
+}

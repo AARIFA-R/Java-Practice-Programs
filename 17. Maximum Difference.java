27c32d22ba6e07e7d9450af05b6e49879e615232
AARIFA R <royalmdn02@gmail.com>
2026-07-23 15:35:46 +0530

    Create 17. Maximum Difference.java

---
+++ 17. Maximum Difference.java
@@ -0,0 +1,16 @@
+public class MaximumDifference {
+    public static void main(String[] args) {
+
+        int[] arr={2,3,10,6,4,8,1};
+
+        int min=arr[0];
+        int maxDiff=0;
+
+        for(int i=1;i<arr.length;i++){
+            maxDiff=Math.max(maxDiff,arr[i]-min);
+            min=Math.min(min,arr[i]);
+        }
+
+        System.out.println(maxDiff);
+    }
+}

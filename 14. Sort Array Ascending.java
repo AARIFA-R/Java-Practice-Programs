1e8eeba2a2611e5dc498d867b481b3201da69c10
AARIFA R <royalmdn02@gmail.com>
2026-07-23 15:34:47 +0530

    Implement array sorting in ascending order

---
+++ 14. Sort Array Ascending.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class AscendingSort {
+    public static void main(String[] args) {
+
+        int[] arr={5,2,1,4,3};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}

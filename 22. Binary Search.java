31374d7df54519b8845b8525aef3f36f5e3c1937
AARIFA R <royalmdn02@gmail.com>
2026-07-23 15:37:31 +0530

    Create 22. Binary Search.java

---
+++ 22. Binary Search.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class BinarySearchDemo {
+    public static void main(String[] args) {
+
+        int[] arr={10,20,30,40,50};
+
+        int key=30;
+
+        int index=Arrays.binarySearch(arr,key);
+
+        System.out.println(index);
+    }
+}
